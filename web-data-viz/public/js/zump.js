d052f02bda13acc6238b8ad999e852f666de242f
Galactiana <ana.barrocal@sptech.school>
2025-09-07 12:03:10 -0300

    Configurando dados do formulário para API backend

--- web-data-viz/public/js/zump.js
+++ web-data-viz/public/js/zump.js
@@ -20,7 +20,18 @@ document.addEventListener("DOMContentLoaded", () =>{
         };  
 
         try{
-            const responde = await fetch('')
+            const responde = await fetch('../usuario/autenticar', {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            });
+
+            
+
+
+
         }
 
 
